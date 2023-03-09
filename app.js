c04406b82167d770032deab2304f5e16067247b0
Somboon Nontaganok <125272647+somboonnontaganok@users.noreply.github.com>
2023-03-09 15:25:17 +0700

    add header and chang id to "message"

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function Hello() {
     alert('Hello, World!');
-    document.getElementById("btn").innerHTML = "The button has been clicked";
+    document.getElementById("message").innerHTML = "The button has been clicked";
     console.log("The button has been clicked");
 }
