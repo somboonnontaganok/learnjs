017ae2b9ef25a3770a09506e3ac42d58b5329e13
Somboon Nontaganok <125272647+somboonnontaganok@users.noreply.github.com>
2023-03-14 11:17:38 +0700

    add min max funtion

---
+++ minMax.js
@@ -0,0 +1,10 @@
+const getRndInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+const x = getRndInt(99, 100);
+const y = getRndInt(1, 50);
+
+// console.log(x);
+// console.log(y);
+
+console.log(randomInput);
