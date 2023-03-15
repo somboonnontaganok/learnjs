8771cf4378ac09b23c863cf08cccb7aa2b188b9c
Somboon Nontaganok <125272647+somboonnontaganok@users.noreply.github.com>
2023-03-15 14:36:41 +0700

    finish array

--- array.js
+++ array.js
@@ -1,11 +1,12 @@
-const numbers = [1,2,3,1,4,5,6];
-  numbers.splice(1,0,'apple',7); //[ 1, 'apple', 7, 2, 3, 4, 5, 6 ]
-  numbers.splice(1,2); //[ 1, 2, 3, 4, 5, 6 ]
+const first = [1,2,3];
+const second = [4,5,6]
+const combine = [...first, ...second];
+console.log(combine);
+const slice = combine.slice(2,4);
+console.log(slice);
 
-//   numbers.length = []; //[]
+const numbers = [3,2,1];
+numbers.sort();
 console.log(numbers);
-// console.log(numbers.indexOf(1));
-// console.log(numbers.lastIndexOf(1));
-console.log(numbers.indexOf('apple') !== -1);
-console.log(numbers.includes(1));
-console.log(numbers.indexOf(1,3)); //find number 1 start from index[3]
+numbers.reverse();
+console.log(numbers);
