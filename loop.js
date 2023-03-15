ba91788cdd10af5f7335ebe0aa715d780bf5879f
Somboon Nontaganok <125272647+somboonnontaganok@users.noreply.github.com>
2023-03-15 14:52:35 +0700

    for loop

---
+++ loop.js
@@ -0,0 +1,12 @@
+// for (let i =1; i <=5; i++ ) {
+//     if(i%2 !=0){
+//         console.log(`${i} am odd number`)
+//     }
+// }
+
+for (let i =1; i <=5; i++ ) {
+    console.log(i); 
+    for(let j=1; j<=5; j++) {
+        console.log('Hello' + j);
+    }
+}
