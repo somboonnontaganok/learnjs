5f9a755d78361f79acd5cd9cf3d4961633e44583
somboonnontaganok <somboonnontaganok@gmail.com>
2023-03-24 07:52:21 +0700

    get hour minut and seccond

--- testE.js
+++ testE.js
@@ -1,8 +1,18 @@
-// use prototype methods
+/* // use prototype methods
 let Storm = function () {};
 Storm.prototype.precip = 'rain';
 let WinterStorm = function () {};
 WinterStorm.prototype = new Storm();
 WinterStorm.prototype.precip = 'snow';
 let bob = new WinterStorm();
-console.log(bob.precip);
+console.log(bob.precip); */
+
+/* // console.log(result);
+let d = new Date;
+let hour = d.getHours();
+let minute = d.getMinutes();
+let second = d.getSeconds();
+// console.log(`${hour}:${minute}:${second}`);
+let y = hour + ':' + minute + ':'+ second; */
+
+
